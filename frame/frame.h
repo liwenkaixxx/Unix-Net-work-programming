53a9205dce50bec9529bc442b6cb981176280aca
wenkai5 <wenkai5@staff.weibo.com>
2021-01-29 11:36:31 +0800

    add file

---
+++ frame/frame.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: frame.h
+	> Created Time: 2016/11/01 18:34:21
+ ************************************************************************/
+
+
+int _main(int argc, char** argv);
+int EnvInitialize(int argc, char** argv, int flag);
+int FrameInitialize();
+int FrameRun();
+int FrameWait();
+int FrameStop();
+int FrameDestroy();
+
+
